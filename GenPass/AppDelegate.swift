991712152e2892dcafe1a30d84f0c9be5ccb6b43
Alex <104495043+AA-4444@users.noreply.github.com>
2024-11-25 07:38:21 +0100

    New strcut of project /new design / adding AppKit / fixing bug that app closing when user press something in it

--- GenPass/AppDelegate.swift
+++ GenPass/AppDelegate.swift
@@ -1,186 +1,40 @@
 //
-//  AppDelegate.swift
+//  Untitled.swift
 //  GenPass
 //
-//  Created by Oleksii Zarytskyi on 11/24/24.
+//  Created by Oleksii Zarytskyi on 11/25/24.
 //
-
-import Cocoa
 import SwiftUI
+import AppKit
 
-class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
-    var statusBarItem: NSStatusItem!
-    var lastGeneratedPassword: String = ""
-    var copyPasswordItem: NSMenuItem!
-    @State private var isOn = false
-    
-    // User settings
-    var passwordLength: Int = 16
-    var includeUppercase: Bool = true
-    var includeNumbers: Bool = true
-    var includeSpecialChars: Bool = true
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem!
+    var popover: NSPopover!
 
-    // MARK: Setup menu bar
-    func setupMenuBar() {
-        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-        
-        if let button = statusBarItem.button {
-            // Icon in the menu bar
-            button.image = NSImage(systemSymbolName: "key.fill", accessibilityDescription: "Password Generator")
-        }
-        
-        let menu = NSMenu()
-
-        // Generate password item
-        let generateItem = NSMenuItem(title: "Generate Password", action: #selector(generatePassword), keyEquivalent: "G")
-        generateItem.target = self
-        menu.addItem(generateItem)
-       
-
-        // Copy password item
-        copyPasswordItem = NSMenuItem(title: "Copy Password", action: #selector(copyPasswordToClipboard), keyEquivalent: "C")
-        copyPasswordItem.isEnabled = false  // Initially disabled
-        copyPasswordItem.target = self
-        menu.addItem(copyPasswordItem)
-
-        // Settings section (password length and options)
-        let settingsTitle = NSMenuItem(title: "Settings", action: nil, keyEquivalent: "")
-        settingsTitle.isEnabled = false // Make header unclickable
-        menu.addItem(settingsTitle)
-
-        // Password Length Menu Item
-        let lengthItem = NSMenuItem(title: "Password Length: \(passwordLength)", action: #selector(changePasswordLength), keyEquivalent: "")
-        lengthItem.target = self
-        menu.addItem(lengthItem)
-        
-        // Add Toggles for options on the right side of the last 3 settings
-        let uppercaseItem = createMacOSToggle(title: "Include Uppercase", state: includeUppercase)
-        uppercaseItem.target = self
-        menu.addItem(uppercaseItem)
-
-        let numbersItem = createMacOSToggle(title: "Include Numbers", state: includeNumbers)
-        numbersItem.target = self
-        menu.addItem(numbersItem)
-
-        let specialCharsItem = createMacOSToggle(title: "Include Special Characters", state: includeSpecialChars)
-        specialCharsItem.target = self
-        menu.addItem(specialCharsItem)
-
-        
-        let quitItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q")
-        quitItem.target = self
-        menu.addItem(quitItem)
-        
-        statusBarItem.menu = menu
-    }
-
-    
-    func createMacOSToggle(title: String, state: Bool) -> NSMenuItem {
-        let toggleItem = NSMenuItem(title: title, action: nil, keyEquivalent: "")
-        
-        
-        let button = NSButton(checkboxWithTitle: title, target: self, action: #selector(toggleSwitch))
-        button.state = state ? .on : .off
-        button.isBordered = false
-        button.font = .systemFont(ofSize: 13)
-        
-        
-        button.setButtonType(.switch)
-        
-      
-        button.sizeToFit()
-        
-        toggleItem.view = button
-        return toggleItem
-    }
-
-   
-    @objc func toggleSwitch(sender: NSButton) {
-        if sender.title == "Include Uppercase" {
-            includeUppercase.toggle()
-        } else if sender.title == "Include Numbers" {
-            includeNumbers.toggle()
-        } else if sender.title == "Include Special Characters" {
-            includeSpecialChars.toggle()
-        }
-        updateMenuItemTitles()
-    }
-
-    
-    @objc func changePasswordLength() {
-        passwordLength = (passwordLength == 16) ? 20 : 16
-        updateMenuItemTitles()
-    }
-
-   
-    @objc func generatePassword() {
-        lastGeneratedPassword = generateRandomPassword()
-        copyPasswordItem.title = "Copy Password: \(lastGeneratedPassword)"
-        copyPasswordItem.isEnabled = true
-    }
-
-    
-    func generateRandomPassword() -> String {
-        var characters = "abcdefghijklmnopqrstuvwxyz"
-        
-        if includeUppercase {
-            characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        }
-        if includeNumbers {
-            characters += "0123456789"
-        }
-        if includeSpecialChars {
-            characters += "!@#$%^&*()"
-        }
-        
-        var password = ""
-        for _ in 0..<passwordLength {
-            let randomIndex = Int.random(in: 0..<characters.count)
-            let char = characters[characters.index(characters.startIndex, offsetBy: randomIndex)]
-            password.append(char)
-        }
-        
-        return password
-    }
-
-    // Copy password to clipboard
-    @objc func copyPasswordToClipboard() {
-        if !lastGeneratedPassword.isEmpty {
-            NSPasteboard.general.clearContents()
-            NSPasteboard.general.setString(lastGeneratedPassword, forType: .string)
-            print("Password copied to clipboard: \(lastGeneratedPassword)")
-        }
-    }
-
-   
-    @objc func quitApp() {
-        NSApplication.shared.terminate(nil)
-    }
-
-    
-    func updateMenuItemTitles() {
-        guard let menu = statusBarItem.menu else { return }
-        
-       
-        for item in menu.items {
-            if item.title.contains("Password Length") {
-                item.title = "Password Length: \(passwordLength)"
-            } else if item.title.contains("Include Uppercase") {
-                item.title = "Include Uppercase: \(includeUppercase ? "Yes" : "No")"
-            } else if item.title.contains("Include Numbers") {
-                item.title = "Include Numbers: \(includeNumbers ? "Yes" : "No")"
-            } else if item.title.contains("Include Special Characters") {
-                item.title = "Include Special Characters: \(includeSpecialChars ? "Yes" : "No")"
-            }
-        }
-    }
+    static let sharedAppState = AppState()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.regular)
-        setupMenuBar() 
-    }
+      
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        statusItem.button?.image = NSImage(systemSymbolName: "key.fill", accessibilityDescription: "Password Generator")
+        statusItem.button?.action = #selector(togglePopover)
 
-    func applicationWillTerminate(_ notification: Notification) {
-        statusBarItem = nil
+        
+        popover = NSPopover()
+        popover.contentViewController = NSHostingController(rootView: GenPassView(appState: AppDelegate.sharedAppState))
+        popover.behavior = .transient
+    }
+  
+
+    
+
+
+    @objc func togglePopover() {
+        guard let button = statusItem.button else { return }
+        if popover.isShown {
+            popover.close()
+        } else {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+        }
     }
 }
--- GenPass/GenPassApp.swift
+++ GenPass/GenPassApp.swift
@@ -1,146 +1,44 @@
-//
-//  GenPassApp.swift
-//  GenPass
-//
-//  Created by Oleksii Zarytskyi on 11/24/24.
-//
+
 import SwiftUI
-import Cocoa
+import AppKit
 
 @main
 struct SafePassApp: App {
-    @State private var passwordLength: Int = 16
-    @State private var includeUppercase: Bool = true
-    @State private var includeNumbers: Bool = true
-    @State private var includeSpecialChars: Bool = true
-    @State private var lastGeneratedPassword: String = ""
-    @State private var passwordHistory: [String] = [] // Store real password history
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
-        MenuBarExtra("Password Generator", systemImage: "key.fill") {
-            VStack {
-                //-- Generate password button
-                Button("Generate Password") {
-                    generatePassword()
-                }
-                .keyboardShortcut("G")
-
-               
-                
-                
-                Button(action: {
-                    copyPasswordToClipboard()
-                }) {
-                    HStack {
-                       
-                        Image(systemName: "arrow.right.doc.on.clipboard")
-                            .foregroundColor(.green)
-                        
-                       
-                        Text("\(lastGeneratedPassword.isEmpty ? "None" : lastGeneratedPassword)")
-                            .lineLimit(1)
-                            .truncationMode(.middle)
-                            .foregroundColor(.green)
-                    }
-                    .padding(.vertical, 5)
-                    .buttonStyle(BorderlessButtonStyle())
-                    
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .disabled(lastGeneratedPassword.isEmpty)
-                .keyboardShortcut("C")
-
-                Divider()
-
-                //-- Password History Section
-                Text("Password History").font(.headline)
-
-                if passwordHistory.isEmpty {
-                    Text("No history yet").font(.subheadline).foregroundColor(.gray)
-                } else {
-                    VStack(spacing: 2) {
-                        ForEach(passwordHistory, id: \.self) { password in
-                            Button(action: {
-                                copyToClipboard(password)
-                            }) {
-                                HStack {
-                                  
-                                    Image(systemName: "arrow.right.doc.on.clipboard")
-                                        .foregroundColor(.green)
-                                    
-                                    
-                                    Text(password)
-                                        .lineLimit(1)
-                                        .truncationMode(.middle)
-                                        .foregroundColor(.green)
-                                }
-                                .padding(.vertical, 5)
-                                .buttonStyle(BorderlessButtonStyle())
-                            }
-                            .frame(maxWidth: .infinity, alignment: .leading)
-
-                            // Remove button for the password
-                            Button(action: {
-                                removePasswordFromHistory(password)
-                            }) {
-                                Text("Delete")
-                                    .foregroundColor(.red)
-                                    .font(.system(size: 12))
-                                    .fontWeight(.semibold)
-                            }
-                            .buttonStyle(BorderlessButtonStyle())
-                        }
-                    }
-                }
-
-                // Clear all history button
-                if !passwordHistory.isEmpty {
-                    Button("Clear All History") {
-                        clearAllHistory()
-                    }
-                    .foregroundColor(.red)
-                    .padding(.top, 5)
-                }
-
-                Divider()
-
-                //-- Settings Section
-                Text("Settings").font(.headline)
-
-                Menu("Password Length: \(passwordLength)") {
-                    Button("10") { changePasswordLength(to: 10) }
-                    Button("16") { changePasswordLength(to: 16) }
-                    Button("20") { changePasswordLength(to: 20) }
-                    Button("25") { changePasswordLength(to: 25) }
-                }
-
-                Toggle("Include Uppercase", isOn: $includeUppercase)
-                Toggle("Include Numbers", isOn: $includeNumbers)
-                Toggle("Include Special Characters", isOn: $includeSpecialChars)
-
-                Divider()
-
-                Button("Quit") {
-                    NSApplication.shared.terminate(nil)
-                }
-            }
-            .padding()
+        Settings {
+            EmptyView()
         }
     }
+}
 
-    // MARK: Generate Password and add the new pass to history
+
+
+
+class AppState: ObservableObject {
+    @Published var passwordLength: Int = 16
+    @Published var includeUppercase: Bool = true
+    @Published var includeNumbers: Bool = true
+    @Published var includeSpecialChars: Bool = true
+    @Published var lastGeneratedPassword: String = ""
+    @Published var passwordHistory: [String] = []
+
     func generatePassword() {
-        lastGeneratedPassword = generateRandomPassword()
+        let password = generateRandomPassword()
+        lastGeneratedPassword = password
 
-        if !lastGeneratedPassword.isEmpty {
-            passwordHistory.insert(lastGeneratedPassword, at: 0)
+        if !password.isEmpty {
+            passwordHistory.insert(password, at: 0)
+            print("Generated Password: \(password)")
+            print("Password History: \(passwordHistory)")
+
             if passwordHistory.count > 5 {
                 passwordHistory.removeLast()
             }
         }
     }
 
-    // MARK: Gen random pass based on user selection
     func generateRandomPassword() -> String {
         var characters = "abcdefghijklmnopqrstuvwxyz"
 
@@ -164,29 +62,18 @@ struct SafePassApp: App {
         return password
     }
 
-    // MARK: Change Pass Length --
-    func changePasswordLength(to newLength: Int) {
-        passwordLength = newLength
-    }
-
-    // MARK: Copy Last Generated Password
     func copyPasswordToClipboard() {
         if !lastGeneratedPassword.isEmpty {
             NSPasteboard.general.clearContents()
             NSPasteboard.general.setString(lastGeneratedPassword, forType: .string)
-            print("Password copied to clipboard: \(lastGeneratedPassword)")
-        } else {
-            print("No password to copy")
         }
     }
 
     func copyToClipboard(_ password: String) {
         NSPasteboard.general.clearContents()
         NSPasteboard.general.setString(password, forType: .string)
-        print("Password copied to clipboard: \(password)")
     }
 
-    // MARK: Remove a Specific Password from History
     func removePasswordFromHistory(_ password: String) {
         if let index = passwordHistory.firstIndex(of: password) {
             passwordHistory.remove(at: index)
@@ -195,5 +82,161 @@ struct SafePassApp: App {
 
     func clearAllHistory() {
         passwordHistory.removeAll()
+    }
+}
+
+
+struct GenPassView: View {
+    @ObservedObject var appState: AppState
+    @State private var isPressed = false
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            //-- Gen Password Section
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Password Generator")
+                    .font(.headline)
+
+                VStack(alignment: .leading, spacing: 5) {
+                    Button("Generate Password") {
+                        appState.generatePassword()
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 10)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+
+                    Button(action: {
+                        appState.copyPasswordToClipboard()
+                    }) {
+                        HStack {
+                            Image(systemName: "arrow.right.doc.on.clipboard")
+                            Text(appState.lastGeneratedPassword.isEmpty ? "None" : appState.lastGeneratedPassword)
+                                .lineLimit(1)
+                                .truncationMode(.middle)
+                                .frame(maxWidth: .infinity, alignment: .leading) // Ensure text is left-aligned
+                        }
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .disabled(appState.lastGeneratedPassword.isEmpty)
+                    .padding(.top, 5)
+                }
+
+              
+            }
+
+            Divider()
+            
+            //-- Password History Section
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Password History")
+                    .font(.headline)
+
+                if appState.passwordHistory.isEmpty {
+                    Text("No history yet")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                } else {
+                   
+                        VStack(alignment: .leading, spacing: 8) {
+                            ForEach(appState.passwordHistory, id: \.self) { password in
+                                HStack {
+                                    
+                                    Button(action: {
+                                        appState.copyToClipboard(password)
+                                    }) {
+                                        HStack {
+                                            Image(systemName: "arrow.right.doc.on.clipboard")
+                                            Text(password)
+                                                .lineLimit(1)
+                                                .truncationMode(.middle)
+                                        }
+                                    }
+                                    .cornerRadius(8)
+                                    
+
+                                    Spacer()
+
+                                    Button(action: {
+                                        appState.removePasswordFromHistory(password)
+                                    }) {
+                                        Text("Delete")
+                                            .foregroundColor(.red)
+                                            .font(.caption)
+                                    }
+                                }
+                                .padding(.vertical, 4)
+                                .background(Color.gray.opacity(0.1))
+                                .cornerRadius(8)
+                            }
+
+                        }
+                    
+                    .frame(maxHeight: 150)
+                }
+
+                if !appState.passwordHistory.isEmpty {
+                    Button("Clear All History") {
+                        appState.clearAllHistory()
+                    }
+                    .foregroundColor(.red)
+                    .padding(.top, 4)
+                }
+            }
+
+            Divider()
+
+            //-- Settings Section
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Settings")
+                    .font(.headline)
+
+                HStack {
+                    Text("Password Length:")
+                    Menu("\(appState.passwordLength)") {
+                        ForEach([10, 16, 20, 25], id: \.self) { length in
+                            Button("\(length)") { appState.passwordLength = length }
+                        }
+                    }
+                }
+                
+                // -- Toggles
+                Toggle("Include Uppercase", isOn: $appState.includeUppercase)
+                    .tint(.blue)
+                Toggle("Include Numbers", isOn: $appState.includeNumbers)
+                    .tint(.blue)
+                Toggle("Include Special Characters", isOn: $appState.includeSpecialChars)
+                    .tint(.blue)
+            }
+
+            Divider()
+
+
+            Button(action: {
+                        NSApplication.shared.terminate(nil)
+                    }) {
+                        Image(systemName: "power.circle")
+                            .symbolRenderingMode(.monochrome)
+                            .foregroundColor(Color.red)
+                            .font(.system(size: 22, weight: .regular))
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                    .scaleEffect(isPressed ? 0.9 : 1.0)
+                    .animation(.spring(response: 0.3, dampingFraction: 0.6), value: isPressed)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .gesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in
+                                isPressed = true
+                            }
+                            .onEnded { _ in
+                                isPressed = false
+                            }
+                    )
+        }
+        .padding()
+        .frame(width: 300)
     }
 }
